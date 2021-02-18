5c52c952200f60d649b920848b7588cdc17fc646
James Small <james.r.small@outlook.com>
2021-02-17 23:39:34 -0500

    Update:  Build paths models/admin

--- paths/admin.py
+++ paths/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Area, Subject, Resource, ResourceType
+
 # Register your models here.
+class ResourceTypeAdmin(admin.ModelAdmin):
+    model = ResourceType
+
+class ResourceAdmin(admin.ModelAdmin):
+    model = Resource
+
+class SubjectAdmin(admin.ModelAdmin):
+    model = Subject
+
+class AreaAdmin(admin.ModelAdmin):
+    model = Area
+
+admin.site.register(ResourceType, ResourceTypeAdmin)
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Area, AreaAdmin)
--- paths/models.py
+++ paths/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class ResourceType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Resource(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    type = models.ForeignKey(
+        ResourceType,
+        on_delete=models.PROTECT,
+        related_name='resources'
+    )
+    url = models.URLField(max_length=2000)
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    resource = models.ForeignKey(
+        Resource,
+        on_delete=models.PROTECT,
+        related_name='subjects'
+    )
+
+    def __str__(self):
+        return self.name
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    resource = models.ForeignKey(
+        Subject,
+        on_delete=models.PROTECT,
+        related_name='areas'
+    )
+
+    def __str__(self):
+        return self.name
