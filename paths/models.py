25762a0279c6b141bef5be4597a90017b5a1fd30
James Small <james.r.small@outlook.com>
2021-02-22 16:41:12 -0500

    Update:  Adjust paths models, build out paths templates
    * paths migration - had one:many direction wrong for models, fixed
    * paths/models.py - flip one:many direction from Areas -> Subjects ->
      Resources
    * templates/base.html - Remove hyperlink from header, allow replacing
      header text, add hyperlink menu-bar
    * templates/path_base.html - Building off base.html, add sub-menu-bar
      for paths application
    * templates/[path|subject|resource]_[list|detail].html - build out list
      and detail templates for each path type - areas, subjects, resources

--- paths/models.py
+++ paths/models.py
@@ -3,6 +3,30 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('path_detail', args=[str(self.id)])
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    area = models.ForeignKey(
+        Area,
+        on_delete=models.PROTECT,
+        related_name='subjects',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
 class ResourceType(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500, blank=True)
@@ -16,6 +40,13 @@ class Resource(models.Model):
     # Don't want to allow Null - want all posts to select a type.  That means
     # we must have a default.  For now, just setting to 1 but there may be a
     # better solution:
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.PROTECT,
+        related_name='resources',
+        null=True,
+        blank=True
+    )
     type = models.ForeignKey(
         ResourceType,
         on_delete=models.PROTECT,
@@ -26,34 +57,3 @@ class Resource(models.Model):
 
     def __str__(self):
         return self.name
-
-class Subject(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    resource = models.ForeignKey(
-        Resource,
-        on_delete=models.PROTECT,
-        related_name='subjects',
-        null=True,
-        blank=True
-    )
-
-    def __str__(self):
-        return self.name
-
-class Area(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    resource = models.ForeignKey(
-        Subject,
-        on_delete=models.PROTECT,
-        related_name='areas',
-        null=True,
-        blank=True
-    )
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('path_detail', args=[str(self.id)])
