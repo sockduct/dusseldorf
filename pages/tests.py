c73a63208facb93452fddbd7c30c116a33aa8868
James Small <james.r.small@outlook.com>
2021-04-07 20:30:10 -0400

    Update:  Finish tests for pages app
    * pages/tests.py - Add tests for ErrorPageView, LeaderboardPageView, and
      SuccessPageView
    * templates - Fix typos found during testing

--- pages/tests.py
+++ pages/tests.py
@@ -2,7 +2,13 @@ from django.contrib.auth import get_user_model
 from django.test import SimpleTestCase, TestCase
 from django.urls import resolve, reverse
 
-from .views import AboutPageView, ContactPageView
+from .views import (
+    AboutPageView,
+    ContactPageView,
+    ErrorPageView,
+    LeaderboardPageView,
+    SuccessPageView
+)
 
 # Create your tests here.
 # Use SimpleTestCase if no database access required
@@ -27,6 +33,7 @@ class AboutPageTests(SimpleTestCase):
         view = resolve('/about/')
         self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
 
+# Use TestCase here as database access required:
 class ContactPageView(TestCase):
     def setUp(self):
         url = reverse('contact')
@@ -73,3 +80,96 @@ class ContactPageView(TestCase):
             'message': 'This is a test form submission...'
         })
         self.assertEqual(response.status_code, 401)
+
+class ErrorPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('error')
+        self.response = self.client.get(url)
+
+    def test_errorpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_errorpage_template(self):
+        self.assertTemplateUsed(self.response, 'error.html')
+
+    def test_errorpage_contains_correct_html(self):
+        self.assertContains(self.response, 'The Developer Nexus - Error')
+
+    def test_errorpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_errorpage_url_resolves_errorpageview(self):
+        view = resolve('/error/')
+        self.assertEqual(view.func.__name__, ErrorPageView.as_view().__name__)
+
+class LeaderboardPageView(TestCase):
+    def setUp(self):
+        # First create users:
+        self.username1 = 'gamma1'
+        self.username2 = 'theta2'
+        self.username3 = 'beta3'
+
+        self.user1 = get_user_model().objects.create_user(
+            username=self.username1,
+            email='gamma1@localhost',
+            password='testpass123'
+        )
+        self.user2 = get_user_model().objects.create_user(
+            username=self.username2,
+            email='theta2@localhost',
+            password='testpass123'
+        )
+        self.user3 = get_user_model().objects.create_user(
+            username=self.username3,
+            email='beta3@localhost',
+            password='testpass123'
+        )
+
+        # Then get web page:
+        url = reverse('leaderboard')
+        self.response = self.client.get(url)
+
+        # Ideally would do test posts/comments too to check scoring functionality,
+        # but I don't want to link to a separate application from this tests
+        # module...
+
+    def test_leaderboardpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_leaderboardpage_template(self):
+        self.assertTemplateUsed(self.response, 'leaderboard.html')
+
+    def test_leaderboardpage_contains_html(self):
+        self.assertContains(self.response, 'The Developer Nexus - Leaderboard')
+        # Ideally would also confirm entries are sorted correctly:
+        self.assertContains(self.response, self.username1)
+        self.assertContains(self.response, self.username2)
+        self.assertContains(self.response, self.username3)
+
+    def test_leaderboardpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_leaderboardpage_url_resolves_leaderboardpageview(self):
+        view = resolve('/leaderboard/')
+        self.assertEqual(view.func.__name__, LeaderboardPageView.__name__)
+
+class SuccessPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('success')
+        self.response = self.client.get(url)
+
+    def test_successpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_successpage_template(self):
+        self.assertTemplateUsed(self.response, 'success.html')
+
+    def test_successpage_contains_correct_html(self):
+        self.assertContains(self.response, 'The Developer Nexus - Success')
+
+    def test_successpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_successpage_url_resolves_successpageview(self):
+        view = resolve('/success/')
+        self.assertEqual(view.func.__name__, SuccessPageView.as_view().__name__)
