4936fa1199d4963e6637ef66befb09d4c04d328c
James Small <james.r.small@outlook.com>
2021-05-19 21:16:52 -0400

    Update:  Fixed sorting on leaderboard page
    * pages/views.py - Adding custom QuerySet to LeaderboardPageView class
      to correctly sort page
    * templates/leaderboard.html - Updated template to leverage custom
      QuerySet.  No need to calculate score, now supplied in passed
      QuerySet.

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from typing import List
 from django.contrib.auth import get_user_model
+from django.db.models import Count, F
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
 from django.views.generic.edit import FormView
@@ -27,6 +28,22 @@ class ErrorPageView(TemplateView):
 
 class LeaderboardPageView(ListView):
     model = get_user_model()
+    '''
+    The following QuerySet is close to this, but not exactly the same:
+    select username, email,
+        ( select 10 * count(*) from posts_post where accounts_customuser.id = posts_post.author_id ) +
+        ( select count(*) from posts_comment where accounts_customuser.id = posts_comment.author_id ) total
+        from accounts_customuser
+        order by total desc, username;
+
+    I believe there are some cases where this won't produce the desired ordering - e.g., 1 post,
+    1 comment would be ordered before 0 posts, 12 comments even though the latter should be first.
+    '''
+    queryset = get_user_model().objects.annotate(
+                post_count=Count('posts'),
+                comment_count=Count('comment'),
+                total=F('post_count') * 10 + F('comment_count')
+                ).order_by('-post_count', '-comment_count', 'username')
     template_name = 'leaderboard.html'
 
 class SuccessPageView(TemplateView):
